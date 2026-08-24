0cab9b07cbe5490b7bda9e2f367f6421388f9065
zeina123445 <daisyzgzg@gmail.com>
2026-08-24 20:28:44 +0300

    bootstraps

---
+++ sec12/data.js
@@ -0,0 +1,28 @@
+// var x = 10 ;
+// var z =12 ;
+// var y = "java";
+// var g = "script";
+// var all = y + g;
+
+
+
+
+// console.log(all)
+// console.log(x+z)
+// console.log(x > z)
+// console.log(x===y)
+// console.log(z!==g)
+// console.log(z===g && y===g)
+// z += 2;
+// console.log(z!=x ||  y===g)
+
+
+var res = prompt('Degree');
+
+if(res > 100)console.log("invalid degree");
+else if(res > 100 ) console.log("invalid degree");
+else if(res >=90 ) console.log("A");
+else if(res >=80 ) console.log("B");
+else if(res >=70 ) console.log("C");
+else if(res == null) alert('please enter degrea');
+    else console.log("f");
