7d738922afa415e6ca036bdbb791646057f9837e
zeina123445 <daisyzgzg@gmail.com>
2026-08-26 20:11:12 +0300

    sec13

---
+++ sec13/fun.js
@@ -0,0 +1,25 @@
+
+// var a = prompt("enter first num")
+// var b = prompt("enter last num")
+// var cont = prompt("enter contNum ")
+// var br = prompt("enter breakNum ")
+
+
+// for(var i = a ; i < b ; i++){
+//    if(i == cont) continue;
+//    else if(i == br) break;
+//    console.log(i);
+// }
+
+let nums = [ "a","b","c"]
+
+
+if(nums.includes(prompt("enter num"))){ 
+alert("founded")}
+
+else{ 
+nums.push(prompt("enter num"))
+alert(nums)
+
+
+}
