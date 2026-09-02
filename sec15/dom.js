31c2aa916b7e23e65ada552adee52b8d3ef6203c
ZeyTrack <zeynepfath1@gmail.com>
2026-09-02 20:26:59 +0300

    tasks

--- sec15/dom.js
+++ sec15/dom.js
@@ -1,3 +1,5 @@
+// task1
+
 let changeColor = (element) => {
 
     let items = document.querySelectorAll("li");
@@ -10,3 +12,42 @@ let changeColor = (element) => {
 
     element.classList.add("bg-danger");
 };
+
+
+// task2
+ 
+let loginform = document.getElementById('loginform')
+
+let users = [];
+
+loginform.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+
+    // console.log('submitted')
+    //  console.log(e)
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    let user = {
+        name: name,
+        email: email
+    };
+    users.push(user);
+    // console.log(users);
+    
+
+    //task3
+    let divsContainer = document.querySelector('.divsContainer');
+    let div =document.createElement('div');
+    let h3 = document.createElement('h3');
+    let p = document.createElement('p');
+    
+    h3.innerText = name;
+    p.innerText = email;
+    div.append(h3)
+    div.append(p)
+    divsContainer.appendChild(div);
+
+    console.log(users);
+});
